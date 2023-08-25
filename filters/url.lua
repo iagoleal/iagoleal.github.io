04329e14c7c72bb6977c6ebabd24f3a5dee51cec
Iago Leal <iago@iagoleal.com>
2023-08-24 21:42:56 -0300

    Adjust url filter for pandoc 3.x version

--- filters/url.lua
+++ filters/url.lua
@@ -3,15 +3,15 @@ local function postfile(fname)
 end
 
 local function template_variable(k)
-  return PANDOC_WRITER_OPTIONS.variables[k]
+  return tostring(PANDOC_WRITER_OPTIONS.variables[k])
 end
 
 function Meta(m)
   if m.url == nil then
-    local fname = PANDOC_STATE.output_file
+    local fname    = postfile(PANDOC_STATE.output_file)
     local sitebase = template_variable("site")
 
-    m.url = pandoc.path.join {sitebase, "posts", postfile(fname)}
+    m.url = pandoc.path.join {sitebase, "posts", fname}
   else
     error "Non-canonical url set on metadata"
   end
